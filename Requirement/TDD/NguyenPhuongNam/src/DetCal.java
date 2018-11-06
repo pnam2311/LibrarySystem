1616ca69d1fc4f4fbc2a11cb2e715579faade5bd
pnam2311@gmail.com <pnam2311@gmail.com>
2018-11-07 02:11:17 +0700

    reupload

--- Requirement/TDD/NguyenPhuongNam/src/DetCal.java
+++
@@ -1,5 +0,0 @@
-public interface DetCal {
-    double DetXCal(double a,double b,double c,double d);
-    double DetYCal(double a,double b,double c,double d);
-    double DetCal(double a,double b,double c,double d);
-}
--- Requirement/pseudocode/entity/Model.java
+++
@@ -1,8 +0,0 @@
-import java.sql.*;
-class Model {
-	protected Connection conn;
-
-	public Model(){
-		conn = DriverManager.getConnection(myUrl, "username", "password");
-	}
-}
